68da5671f6bc4b9c94e78f92c93098ab1a3aca63
Archit Khanna <architk.222@gmail.com>
2020-06-03 11:25:48 +0100

    Profile Picture Added
    - Profile pictures added as a feature;
    - They show up on the profile, the announcement;
    - They are also resized upon upload;

--- HousekeepingApp/users/models.py
+++ HousekeepingApp/users/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     mobile = PhoneNumberField(blank=False, null=False)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
